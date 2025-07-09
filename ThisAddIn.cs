86845c8ce35af3aac6d0476d3c49fbe69b23453c
janette-jerusal <janette.jerusal2@rtx.com>
2025-07-09 11:02:10 -0400

    Create ThisAddIn.cs

---
+++ ThisAddIn.cs
@@ -0,0 +1,35 @@
+using System;
+using Microsoft.Office.Tools.Excel;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace UserStorySimilarityAddIn
+{
+    public partial class ThisAddIn
+    {
+        private MyRibbon ribbon;
+
+        private void ThisAddIn_Startup(object sender, System.EventArgs e)
+        {
+        }
+
+        private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
+        {
+        }
+
+        protected override Microsoft.Office.Core.IRibbonExtensibility CreateRibbonExtensibilityObject()
+        {
+            ribbon = new MyRibbon();
+            return ribbon;
+        }
+
+        #region VSTO generated code
+
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisAddIn_Startup);
+            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+        }
+
+        #endregion
+    }
+}
