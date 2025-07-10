695c1e31a0dd47ba37ebf6c2477b298c5a09bb8a
janette-jerusal <janette.jerusal2@rtx.com>
2025-07-10 10:07:49 -0400

    Update MyRibbon.cs

--- MyRibbon.cs
+++ MyRibbon.cs
@@ -11,47 +11,44 @@ namespace UserStorySimilarityAddIn
 
         public MyRibbon()
         {
-            // Confirm constructor is triggered
             MessageBox.Show("Ribbon Constructor Called");
         }
 
         public string GetCustomUI(string ribbonID)
         {
-            MessageBox.Show("GetCustomUI TRIGGERED");
+            MessageBox.Show("GetCustomUI triggered!");
 
-            try
+            var assembly = Assembly.GetExecutingAssembly();
+            var resources = assembly.GetManifestResourceNames();
+            MessageBox.Show("Resources:\n" + string.Join("\n", resources));
+
+            foreach (var res in resources)
             {
-                var assembly = Assembly.GetExecutingAssembly();
-                MessageBox.Show("Assembly Loaded");
-
-                var resources = assembly.GetManifestResourceNames();
-                MessageBox.Show("Resources:\n" + string.Join("\n", resources));
-
-                using (Stream stream = assembly.GetManifestResourceStream("UserStorySimilarityAddIn.MvRibbon.xml"))
+                if (res.Contains("MvRibbon"))
                 {
-                    if (stream == null)
-                    {
-                        MessageBox.Show("MvRibbon.xml not found!");
-                        return null;
-                    }
-
+                    using (Stream stream = assembly.GetManifestResourceStream(res))
                     using (StreamReader reader = new StreamReader(stream))
                     {
-                        return reader.ReadToEnd();
+                        var xml = reader.ReadToEnd();
+                        MessageBox.Show("Ribbon XML loaded:\n\n" + xml);
+                        return xml;
                     }
                 }
             }
-            catch (System.Exception ex)
-            {
-                MessageBox.Show("Error loading ribbon: " + ex.Message);
-                return null;
-            }
+
+            MessageBox.Show("MvRibbon.xml not found in embedded resources.");
+            return null;
         }
 
         public void OnLoad(IRibbonUI ribbonUI)
         {
             this.ribbon = ribbonUI;
         }
+
+        // 🔘 This connects to the onAction="OnCompareClick" in your XML
+        public void OnCompareClick(IRibbonControl control)
+        {
+            MessageBox.Show("Compare button clicked!");
+        }
     }
 }
-
